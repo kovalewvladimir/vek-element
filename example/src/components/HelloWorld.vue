01ad8641d9a3e28814c7b987ed611f3901600983
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-10-18 09:40:52 +0000

    New: Init repo

---
+++ example/src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { VueComponentNpmExample } from 'my-components';
+</script>
+
+<template>
+  <VueComponentNpmExample msg="123"></VueComponentNpmExample>
+</template>
+
