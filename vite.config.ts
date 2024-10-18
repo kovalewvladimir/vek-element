7905abdcac59418793092e907cf721aea9064dbe
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-10-18 09:50:19 +0000

    Fix: Output bundle formats es

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
 
@@ -10,16 +9,12 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       name: 'VueComponentNpmExample',
-      // the proper extensions will be added
-      fileName: 'vue-component-npm-example'
+      fileName: 'vue-component-npm-example',
+      formats: ['es']
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue'
         }
