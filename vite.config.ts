a5e81b4b29b51c71aeb385e73da990d62519baea
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-10-21 12:33:38 +0000

    Fix: Vite dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+    port: 8999,
+  },
 
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      fileName: 'my-components',
-      formats: ['es']
+      entry: resolve(__dirname, "src/lib/index.ts"),
+      fileName: "my-components",
+      formats: ["es"],
     },
-    
+
     sourcemap: true,
 
     rollupOptions: {
       // Библиотека, которую мы хотим использовать в качестве внешней зависимости
-      external: ['vue']
-    }
-  }
-})
+      external: ["vue"],
+    },
+  },
+
+  plugins: [vue()],
+
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "./src") },
+      {
+        find: "my-components",
+        replacement: resolve(__dirname, "./src/lib/index.ts"),
+      },
+    ],
+  },
+});
