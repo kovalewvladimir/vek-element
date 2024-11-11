2df7f5bc5c9bd3d6ec47f07a842e411dd7412a8b
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-11-11 07:02:21 +0000

    New: Add vue-router and delete vite-plugin-checker

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -3,26 +3,10 @@ import { resolve } from 'path'
 import UnoCSS from 'unocss/vite'
 import ElementPlus from 'unplugin-element-plus/vite'
 import { defineConfig } from 'vite'
-import Checker from 'vite-plugin-checker'
 import SvgLoader from 'vite-svg-loader'
 
 export default defineConfig({
-  plugins: [
-    Vue(),
-
-    ElementPlus({}),
-    UnoCSS(),
-
-    SvgLoader(),
-
-    Checker({
-      vueTsc: true,
-      eslint: {
-        useFlatConfig: true,
-        lintCommand: 'eslint'
-      }
-    })
-  ],
+  plugins: [Vue(), ElementPlus({}), UnoCSS(), SvgLoader()],
 
   server: {
     host: '0.0.0.0',
