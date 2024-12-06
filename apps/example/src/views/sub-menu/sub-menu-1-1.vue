80cfdca003e342e24d0f5b840bdc9a2148a67041
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-12-06 07:06:32 +0000

    Fix: Добавлены компоненты ввода в подменю 1-1 и 1-2 для проверки cache

--- apps/example/src/views/sub-menu/sub-menu-1-1.vue
+++ apps/example/src/views/sub-menu/sub-menu-1-1.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { ElInput } from 'element-plus'
+import { ref } from 'vue'
+
+const input = ref('')
+</script>
+
 <template>
   <h1>Sub Menu 1-1</h1>
+
+  <el-input
+    v-model="input"
+    placeholder="test"
+  />
 </template>
