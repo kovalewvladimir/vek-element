e1d646db9143033514f87de1f03f22dac726261b
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-12-24 09:52:19 +0000

    Fix: Добавлена функция синхронной задержки в утилиты

--- libs/ui/src/utils/sleep.ts
+++ libs/ui/src/utils/sleep.ts
@@ -5,3 +5,16 @@
  * @returns Promise, который разрешается после задержки
  */
 export const asyncSleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+/**
+ * Функция для синхронной задержки
+ *
+ * @param ms - количество миллисекунд для задержки
+ */
+export const syncSleep = (ms: number): void => {
+  const date = Date.now()
+  let currentDate: number
+  do {
+    currentDate = Date.now()
+  } while (currentDate - date < ms)
+}
