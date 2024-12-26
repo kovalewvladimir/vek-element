ad6ec74d0a9b2aefe393219d7b5820e56301c370
kovalewvladimir <kovalew.vladimir@gmail.com>
2024-12-26 05:53:11 +0000

    Fix: Добавлена функция обработки неудачного входа с задержкой

--- apps/example/src/views/login/the-login.vue
+++ apps/example/src/views/login/the-login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useLoading } from '@vek-element/ui'
+import { asyncSleep } from '@vek-element/ui/utils'
 import { ElButton, ElLink } from 'element-plus'
 import { useRoute, useRouter } from 'vue-router'
 
@@ -19,6 +20,11 @@ const loginClk = loadingWrapper(async () => {
     return
   }
   await router.push('/')
+})
+
+const loginClkFail = loadingWrapper(async () => {
+  await asyncSleep(1000)
+  throw new Error('login fail')
 })
 </script>
 
@@ -43,6 +49,12 @@ const loginClk = loadingWrapper(async () => {
       :loading="loading"
       @click="loginClk"
       >Login</el-button
+    >
+
+    <el-button
+      :loading="loading"
+      @click="loginClkFail"
+      >loginClkFail</el-button
     >
   </div>
 </template>
