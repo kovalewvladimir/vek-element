23d52a25cd9f89572ed66bab8c11e9c179ac30e4
kovalewvladimir <kovalew.vladimir@gmail.com>
2025-01-29 10:58:41 +0000

    Fix: Экспортирован тип IOnLoadDataParams

--- libs/ui/src/components/virt-table/index.ts
+++ libs/ui/src/components/virt-table/index.ts
@@ -2,6 +2,7 @@ import type VirtTable from './src/virt-table.vue'
 
 type VuVirtTableInstance = InstanceType<typeof VirtTable>
 
+export type { IOnLoadDataParams } from './src/types'
 export type { VuVirtTableInstance }
 export { Column, Columns, type IColumn } from './src/types'
 export { default as VuVirtTable } from './src/virt-table.vue'
