d87f5aea08541b0043ad2cf2c0a17ab92301becb
kovalewvladimir <kovalew.vladimir@gmail.com>
2025-10-03 12:26:58 +0000

    Fix: Упорядочить разметку и улучшить структуру компонента VuButtonIcon

--- apps/example/src/views/vu-components/the-vu-button-icon.vue
+++ apps/example/src/views/vu-components/the-vu-button-icon.vue
@@ -13,84 +13,94 @@ const types: VuButtonIconType[] = ['default', 'success', 'warning', 'info', 'pri
       <h2>vu-button-icon</h2>
     </template>
 
-    <el-divider content-position="left">Аттрибут tooltip</el-divider>
-    <div
-      v-for="icon in icons"
-      :key="icon"
-      class="mb-10px"
-    >
-      <vu-button-icon
-        v-for="type in types"
-        :key="type"
-        :type="type"
-        :icon="icon"
-        :tooltip="`Type: ${type}, Icon: ${icon}`"
-      />
+    <div class="flex gap-5">
+      <div class="flex-1">
+        <el-divider content-position="left">Аттрибут tooltip</el-divider>
+        <div
+          v-for="icon in icons"
+          :key="icon"
+          class="mb-10px"
+        >
+          <vu-button-icon
+            v-for="type in types"
+            :key="type"
+            :type="type"
+            :icon="icon"
+            :tooltip="`Type: ${type}, Icon: ${icon}`"
+          />
+        </div>
+      </div>
+
+      <div class="flex-1">
+        <el-divider content-position="left">С текстом</el-divider>
+        <div
+          v-for="icon in icons"
+          :key="icon"
+          class="mb-10px"
+        >
+          <vu-button-icon
+            v-for="type in types"
+            :key="type"
+            :type="type"
+            :icon="icon"
+            >{{ type }}</vu-button-icon
+          >
+        </div>
+      </div>
     </div>
 
-    <el-divider content-position="left">Аттрибут text</el-divider>
-    <div
-      v-for="icon in icons"
-      :key="icon"
-      class="mb-10px"
-    >
-      <vu-button-icon
-        v-for="type in types"
-        :key="type"
-        text
-        :type="type"
-        :icon="icon"
-        :tooltip="`Type: ${type}, Icon: ${icon}`"
-      />
-    </div>
+    <div class="flex gap-5">
+      <div class="flex-1">
+        <el-divider content-position="left">Аттрибут text</el-divider>
+        <div
+          v-for="icon in icons"
+          :key="icon"
+          class="mb-10px"
+        >
+          <vu-button-icon
+            v-for="type in types"
+            :key="type"
+            text
+            :type="type"
+            :icon="icon"
+          />
+        </div>
+      </div>
 
-    <el-divider content-position="left">Аттрибут text bg</el-divider>
-    <div
-      v-for="icon in icons"
-      :key="icon"
-      class="mb-10px"
-    >
-      <vu-button-icon
-        v-for="type in types"
-        :key="type"
-        text
-        bg
-        :type="type"
-        :icon="icon"
-        :tooltip="`Type: ${type}, Icon: ${icon}`"
-      />
-    </div>
+      <div class="flex-1">
+        <el-divider content-position="left">Аттрибут text bg</el-divider>
+        <div
+          v-for="icon in icons"
+          :key="icon"
+          class="mb-10px"
+        >
+          <vu-button-icon
+            v-for="type in types"
+            :key="type"
+            text
+            bg
+            :type="type"
+            :icon="icon"
+          />
+        </div>
+      </div>
 
-    <el-divider content-position="left">Аттрибут link</el-divider>
-    <div
-      v-for="icon in icons"
-      :key="icon"
-      class="mb-10px"
-    >
-      <vu-button-icon
-        v-for="type in types"
-        :key="type"
-        link
-        :type="type"
-        :icon="icon"
-        :tooltip="`Type: ${type}, Icon: ${icon}`"
-      />
-    </div>
-
-    <el-divider content-position="left">С текстом</el-divider>
-    <div
-      v-for="icon in icons"
-      :key="icon"
-      class="mb-10px"
-    >
-      <vu-button-icon
-        v-for="type in types"
-        :key="type"
-        :type="type"
-        :icon="icon"
-        :tooltip="`Type: ${type}, Icon: ${icon}`"
-        >{{ type }}</vu-button-icon
-      >
+      <div class="flex-1">
+        <el-divider content-position="left">Аттрибут link</el-divider>
+        <div
+          v-for="icon in icons"
+          :key="icon"
+          class="mb-10px"
+        >
+          <vu-button-icon
+            v-for="type in types"
+            :key="type"
+            link
+            :type="type"
+            :icon="icon"
+          />
+        </div>
+      </div>
     </div>
   </vu-content-wrap>
 </template>
